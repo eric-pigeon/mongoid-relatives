97dc581c584e0d79e54005572773823e6751f146
epigeon <epigeon@weblinc.com>
2015-04-21 17:44:20 -0400

    fix typo

--- lib/mongoid/relatives/relations/builders/relates/in.rb
+++ lib/mongoid/relatives/relations/builders/relates/in.rb
@@ -9,7 +9,7 @@ module Mongoid
             def build(type = nil)
               return object unless query?
               model = type ? type.constanttize : metadata.klass
-              metedata.criteria(object,model).first
+              metadata.criteria(object,model).first
             end
           end
         end
